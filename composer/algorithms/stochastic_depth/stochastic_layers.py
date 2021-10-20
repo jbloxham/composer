0fa815b03faad639621dd73454c0cb26437e473f
Landan Seguin <landanjs@gmail.com>
2021-10-20 17:48:48 +0000

    Fix bernoulli probability

--- composer/algorithms/stochastic_depth/stochastic_layers.py
+++ composer/algorithms/stochastic_depth/stochastic_layers.py
@@ -152,7 +152,7 @@ class StochasticGPT2Block(GPT2Block):
     ):
 
         residual = hidden_states
-        sample = torch.bernoulli(self.drop_rate)
+        sample = torch.bernoulli(1 - self.drop_rate)
         if not self.training and sample:
             hidden_states = self.ln_1(hidden_states)
             attn_outputs = self.attn(
